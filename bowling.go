301dbc51b8e738dc34cad7b97a35b446fee906e5
jasbrake <jasbrake@gmail.com>
2020-05-15 23:45:42 -0400

    Clarified bonus's range in a comment

--- bowling.go
+++ bowling.go
@@ -15,7 +15,7 @@ const StrikeScore = 10
 // The game is passed in as a string of frames each separated by a '-' (without
 // quotation marks). Strikes are represented with an 'X', spares are represented
 // with a '/', and an open frame is two digits representing the number of pins
-// knocked down by the 2 throws in that frame.
+// knocked down by the two throws in that frame.
 //
 // We assume that the game string passed in is a valid game.
 func ScoreGame(game string) int {
@@ -23,6 +23,7 @@ func ScoreGame(game string) int {
 	// the number of throws so far
 	throw := 0
 	// bonus tells us whether we apply a bonus from a previous strike or spare
+	// and will always fall in the range [0,3]
 	bonus := 0
 	// used to calculate a spare's point value easily
 	prevNumber := 0
@@ -57,8 +58,7 @@ func ScoreGame(game string) int {
 		}
 
 		if bonus > 0 {
-			// there's a 2x bonus if the previous 2 throws were strikes
-			// the bonus counter will only ever be 0, 1, 2, or 3
+			// there's a 2x bonus if the previous two throws were strikes
 			if bonus >= 3 {
 				total += 2 * num
 				bonus -= 2
